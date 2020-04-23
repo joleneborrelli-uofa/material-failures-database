d557e9d1f5d71a07da6558ea71e1c06ed24e4e6b
Jolene Borrelli <Jaomin@j-bs-macbook.local>
2020-04-23 17:09:07 -0600

    Provide database response to index.js only

--- src/index.js
+++ src/index.js
@@ -1,26 +1,10 @@
 import { displayHeaders, unknownObject } from './constants.js';
 import { generateRecordHtml }            from './templates/record.js';
 import { generateCaseStudyHtml }         from './templates/caseStudy.js';
-import { database } 				     from './database/record.js';
 
-/**
-	@function buildHeaders
-	
-	@arg database : { Object } 
-	@return       : { Object }     
-*/
-const buildHeaders = () =>
-{
-	let headers        = { ...displayHeaders };
-	const databaseKeys = Object.keys( database.object );
-
-	for( let key in headers )
-	{
-		if( !databaseKeys.includes( key ) ) delete headers[key]
-	}
-
-	return headers;
-};
+// Will eventually be a singular database response
+import { recordDatabase }    from '../database/record.js';
+import { caseStudyDatabase } from '../database/caseStudy.js';
 
 /**
 	@function createHtml
@@ -34,18 +18,10 @@ const createHtml = ( type ) =>
 
 	if( type !== 'caseStudy' || type !== 'record' ) type = 'record';
 
-	if( database.hasOwnProperty( 'object' ) )
-	{
-		let innerHtml;
+	if ( type === 'caseStudy' ) innerHtml = generateCaseStudyHtml( caseStudyDatabase );
+	if ( type === 'record' )    innerHtml = generateRecordHtml( recordDatabase );
 
-		const title     = database.object.name || unknownObject;
-		const headers   = buildHeaders();
-
-		if ( type === 'caseStudy' ) innerHtml = generateCaseStudyHtml( headers, title );
-		if ( type === 'record' )    innerHtml = generateRecordHtml( headers, title );
-
-		child.innerHTML = innerHtml;
-	}
+	child.innerHTML = innerHtml;
 
 	return child;
 };
