e93e0da17b215d7dd969aa3748362ae89ac9daaf
Jolene Borrelli <Jaomin@j-bs-macbook.local>
2020-04-23 13:37:28 -0600

    Remove enclosing object field from record and caseStudy database response

---
+++ src/database/caseStudy.js
@@ -0,0 +1,40 @@
+export const database =
+{
+
+    "object" :
+    {
+        "id"   : "1",
+        "name" : "Bob the Builder Plate",
+    },
+
+    "material" :
+    {
+        "additional_prompt" : 
+        [
+            "Thinking about the material family, would you agree that the plate is made of both the materials stated in the response from the manufacturer? Why or why not?"
+        ]
+    },
+
+    "environment" :
+    {
+        "geographic_location" : "Edmonton, AB, Canada",
+        "location"            : [ 
+                                    {
+                                        "location_type"        : "Indoor",
+                                        "location_description" : "Kitchen and/or dining room."
+                                    } ]
+    },
+
+    "use" :
+    {
+        "problem_statement" : "The Bob the Builder Plate was removed from service. It experienced catastrophic structural failure with warping and fracture. The causes of warping and fracture are unknown.",
+        "record"            : [
+                                {
+                                    "record_type"        : "Maintenance",
+                                    "record_description" : "Cleaned in dishwasher."
+                                } ]
+    },
+
+    "references" : []
+
+}
