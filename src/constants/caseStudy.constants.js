ee253d13448a9bb9de9f2ff94a5654cc201991b5
Jolene Borrelli <Jaomin@j-bs-macbook.local>
2020-04-23 20:55:19 -0600

    Added comments for missing case study prompts

--- src/constants/caseStudy.constants.js
+++ src/constants/caseStudy.constants.js
@@ -20,10 +20,11 @@ export const caseStudy =
 
         object :
         {
+            // rating
             inscription   : 'Are there any inscriptions on the plate? What do they tell you about the plate?',
-            recyclability : 'Is the plate recyclable? Does the recyclability tell you something about the material?',
             fractography  : 'Are there fractures in the plate? Draw the plate and the fractures. Determine the fracture origin(s).',
             feature       : 'What physical features are there on the plate?'
+            // serial_number
         },
 
         material :
@@ -32,28 +33,48 @@ export const caseStudy =
             crystallinity : 'What kind of crystal structure does the material have?',
             'class'       : 'What class of material is it?',
             subclass      : 'Given the material family, what is the subclass of material?'
+            recyclability : 'Is the plate recyclable? Does the recyclability tell you something about the material?'
+            // biodegradability
+            // alloy_designation
+            // grade
+            // toxicity
+
         },
 
         processing :
         {
-            treatment       : 'What treatment procedures did the plate have?',
-            procedure       : 'What shaping procedures did the plate have?', 
-            residual_stress : 
-            {
-                type  : 'What kind of residual stresses could have been introduced during processing?',
-                force : 'What kind of forces could have been created in the plate from the residual stresses? Did the physical features on the plate play a role?'
-            }
+            treatment             : 'What treatment procedures did the plate have?',
+            procedure             : 'What shaping procedures did the plate have?', 
+            residual_stress       : 'What kind of residual stresses could have been introduced during processing?',
+            residual_stress_force : 'What kind of forces could have been created in the plate from the residual stresses? Did the physical features on the plate play a role?'
+            // manufacturer
+            // date
+            // location
+            // plant
         },
 
         environment :
         {
             loading : 'What type of loading did the plate experience?'
+            // loading_normalcy 
+            // loading_frequency
+            // ambient
+            // light_exposure
+            // geographic_location
+            // stress_orientation
         },
 
         use :
         {
             functionality_loss : 'What functionality was lost when the plate failed?',
             operational_factor : 'Which factors played a part in the plate’s failure during operation or maintenance?'
+            // problem_statement
+            // failure_time
+            // failure_operation_stage
+            // specification
+            // record
+            // operation
+            // operator
         },
 
         fault_tree :
