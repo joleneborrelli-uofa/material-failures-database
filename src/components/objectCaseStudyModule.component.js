b62411908532dbc5d3326a854e8febe501401a8b
Jolene Borrelli <Jaomin@j-bs-macbook.local>
2020-04-29 19:20:42 -0600

    Issue #2 Add objectCaseStudyModule component

---
+++ src/components/objectCaseStudyModule.component.js
@@ -0,0 +1,75 @@
+import { caseStudy }          from '../constants/caseStudy.constants.js';
+import { caseStudyHtmlClass } from '../constants/htmlClass.constants.js';
+import { foreignKeys }        from '../constants/foreignKey.constants.js';
+import PromptTextArea         from './promptTextArea.components.js'; 
+import PromptTitle            from './promptTitle.components.js'; 
+
+class ObjectCaseStudyModule extends React.component
+{
+    constructor( props )
+    {
+        super( props );
+
+        this.state =
+        {
+            rating        : '',
+            inscription   : '',
+            serial_number : '',
+            feature       : [],
+            feature_note  : ''
+        }
+    }
+
+    render()
+    {
+        const constants = caseStudy.fieldPrompts.object;
+        const htmlClass = caseStudyHtmlClass.fieldPrompts;
+
+        return (
+            `<div className="${ htmlClass.object }">
+
+                <PromptTitle 
+                    title="${ constants.rating }" 
+                    visibility={ this.props.visibility.rating } >
+                    <PromptTextArea 
+                        name="rating"
+                        labelVisibility="${ htmlClass.visibility.off }"
+                        handleChange={ this.props.handleObjectModuleChange } />
+                </PromptTitle>
+
+                <PromptTitle  
+                    title="${ constants.inscription }" 
+                    visibility={ this.props.visibility.inscription }>
+                    <PromptTextArea 
+                        name="inscription"
+                        labelVisibility="${ htmlClass.visibility.off }"
+                        handleChange={ this.props.handleObjectModuleChange } />
+                </PromptTitle>
+
+                <PromptTitle  
+                    title="${ constants.serial_number }" 
+                    visibility={ this.props.visibility.serial_number }>
+                    <PromptTextArea 
+                        name="serial_number"
+                        labelVisibility="${ htmlClass.visibility.off }"
+                        handleChange={ this.props.handleObjectModuleChange } />
+                </PromptTitle>
+
+                <PromptTitle  
+                    title="${ constants.feature }" 
+                    visibility={ this.props.visibility.feature }>
+                    <PromptCheckboxGroup
+                        name="feature"
+                        handleChange={ this.props.handleObjectModuleChange }
+                        foreignKeys={ foreignKeys.object.feature } />
+                    <PromptTextArea 
+                        name="feature_note"
+                        labelVisibility="${ htmlClass.visibility.on }"
+                        handleChange={ this.props.handleObjectModuleChange } />
+                </PromptTitle>
+
+            </div>`
+        )
+    }
+
+};
