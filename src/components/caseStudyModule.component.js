63389d5b7c122d7bdc7dc29d1a06c2aa073e7aa0
Jolene Borrelli <Jaomin@j-bs-macbook.local>
2020-04-29 20:05:07 -0600

    Issue #7 Add the MaterialCaseStudyModule to the CaseStudyModule

--- src/components/caseStudyModule.component.js
+++ src/components/caseStudyModule.component.js
@@ -10,34 +10,48 @@ class CaseStudyModule extends React.component
 
         this.state =
         {
-            rating        : '',
-            inscription   : '',
-            serial_number : '',
-            feature       : new Map(),
-            feature_note  : ''
+            rating            : '',
+            inscription       : '',
+            serial_number     : '',
+            feature           : new Map(),
+            feature_note      : '',
+            name              : '',
+            'class'           : '',
+            class_subtype     : '',
+            class_note        : '',
+            crystallinity     : '',
+            alloy_designation : '',
+            grade             : '',
+            recyclability     : '',
+            biodegradability  : '',
+            toxicity          : ''
         }
 
+        this.handleGenericChange      = this.handleGenericChange.bind( this );
         this.handleObjectModuleChange = this.handleObjectModuleChange.bind( this );
     }
 
-    handleObjectModuleChange( e )
+    handleGenericChange( e )
     {
         const name = e.target.name;
-
-        if( name === 'feature' )
-        {
-            const isChecked = e.target.checked;
-
-            return this.setState( prevState =>
-            ( {
-                feature : prevState.feature.set( name, isChecked ) 
-            } ) );
-        }
 
         this.setState(
         {
             [name]: e.target.value
         } );
+    }
+
+    handleObjectModuleChange( e )
+    {
+        if( e.target.name === 'feature' )
+        {
+            return this.setState( prevState =>
+            ( {
+                feature : prevState.feature.set( name, e.target.checked ) 
+            } ) );
+        }
+
+        this.handleGenericChange( e );
     }
 
     render()
@@ -50,6 +64,10 @@ class CaseStudyModule extends React.component
                 <ObjectCaseStudyModule 
                     visibility={ this.props.visibility.object }
                     handleObjectModuleChange={ this.handleObjectModuleChange } />
+
+                <MaterialCaseStudyModule 
+                    visibility={ this.props.visibility.material }
+                    handleMaterialModuleChange={ this.handleGenericChange } />
 
             </div>`
         )
