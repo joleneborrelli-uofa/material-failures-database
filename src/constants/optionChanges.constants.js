162558fcfb4ca47ee28b13bc368e9d1d8c5af0bd
Jolene Borrelli <Jaomin@j-bs-macbook.local>
2020-05-01 14:07:49 -0600

    Constants for option changes in grouped components

---
+++ src/constants/optionChanges.constants.js
@@ -0,0 +1,28 @@
+export const optionChanges = 
+{
+    processing :
+    {
+        checkbox:
+        [
+            'machining', 
+            'joining'
+        ],
+
+        radio :
+        [
+            'treatment', 
+            'treatment_subtype', 
+            'shaping',
+            'shaping_subtype',
+            'residual_stress',
+            'residual_stress_force'
+        ],
+
+        button :
+        [
+            'treatment',
+            'shaping',
+            'residual_stress'
+        ]
+    }
+};
