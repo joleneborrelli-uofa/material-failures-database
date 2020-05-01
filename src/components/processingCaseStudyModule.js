e3a111f50839cc9675b42e8de8c2ef90f4817138
Jolene Borrelli <Jaomin@j-bs-macbook.local>
2020-05-01 12:56:20 -0600

    Issue #9 Moved this.props to const, changed pairedRadioGroup props

--- src/components/processingCaseStudyModule.js
+++ src/components/processingCaseStudyModule.js
@@ -1,4 +1,4 @@
-import { treatment, shaping, residualStress } from '../constants/groupedComponents.constants.js';
+import { groupedComponents }  from '../constants/groupedComponents.constants.js';
 import { caseStudy }          from '../constants/caseStudy.constants.js';
 import { caseStudyHtmlClass } from '../constants/htmlClass.constants.js';
 import { foreignKeys }        from '../constants/foreignKey.constants.js';
@@ -15,6 +15,13 @@ class ProcessingCaseStudyModule extends React.component
 
     render()
     {
+        const 
+        {
+            state,
+            visibility,
+            handleProcessingModuleChange
+        } = this.props;
+
         const constants = caseStudy.fieldPrompts.processing;
         const htmlClass = caseStudyHtmlClass.fieldPrompts;
 
@@ -23,137 +30,131 @@ class ProcessingCaseStudyModule extends React.component
 
                 <PromptTitle 
                     title="${ constants.treatment }" 
-                    visibility={ this.props.visibility.treatment }>
+                    visibility={ visibility.treatment }>
 
                     <PromptPairedRadioGroupList
                         key="treatment"
-                        button="treatment_button"
-                        pairedRadioGroup={ this.props.treatment }
-                        handleChange={ this.props.handleProcessingModuleChange }
-                        firstRadioGroup={ treatment.firstRadioGroup }
-                        secondRadioGroup={ treatment.secondRadioGroup } />
+                        pairedRadioGroup={ state.treatment }
+                        pairedData={ groupedComponents.treatment }
+                        handleChange={ handleProcessingModuleChange } />
 
                     <PromptTextArea 
                         name="treatment_note"
-                        value={ this.props.state.treatment_note }
+                        value={ state.treatment_note }
                         labelVisibility="${ htmlClass.visibility.on }"
-                        handleChange={ this.props.handleProcessingModuleChange } />
+                        handleChange={ handleProcessingModuleChange } />
                 </PromptTitle>
 
                 <PromptTitle 
                     title="${ constants.shaping }" 
-                    visibility={ this.props.visibility.shaping }>
+                    visibility={ visibility.shaping }>
 
                     <PromptPairedRadioGroupList
                         key="shaping"
-                        button="shaping_button"
-                        pairedRadioGroup={ this.props.shaping }
-                        handleChange={ this.props.handleProcessingModuleChange }
-                        firstRadioGroup={ shaping.firstRadioGroup }
-                        secondRadioGroup={ shaping.secondRadioGroup } />
+                        pairedRadioGroup={ state.shaping }
+                        pairedData={ groupedComponents.shaping } 
+                        handleChange={ handleProcessingModuleChange } />
 
                     <PromptTextArea 
                         name="shaping_note"
-                        value={ this.props.state.shaping_note }
+                        value={ state.shaping_note }
                         labelVisibility="${ htmlClass.visibility.on }"
-                        handleChange={ this.props.handleProcessingModuleChange } />
+                        handleChange={ handleProcessingModuleChange } />
                 </PromptTitle>
 
                 <PromptTitle 
                     title="${ constants.residual_stress }" 
-                    visibility={ this.props.visibility.residualStress }>
+                    visibility={ visibility.residualStress }>
 
                     <PromptPairedRadioGroupList
                         key="residual_stress"
-                        button="residual_stress_button"
-                        pairedRadioGroup={ this.props.residual_stress }
-                        handleChange={ this.props.handleProcessingModuleChange }
-                        firstRadioGroup={ residual_stress.firstRadioGroup }
-                        secondRadioGroup={ residual_stress.secondRadioGroup } />
+                        pairedRadioGroup={ state.residual_stress }
+                        pairedData={ groupedComponents.treatment } 
+                        handleChange={ handleProcessingModuleChange } />
 
                     <PromptTextArea 
                         name="residual_stress_note"
-                        value={ this.props.state.residual_stress_note }
+                        value={ state.residual_stress_note }
                         labelVisibility="${ htmlClass.visibility.on }"
-                        handleChange={ this.props.handleProcessingModuleChange } />
+                        handleChange={ handleProcessingModuleChange } />
                 </PromptTitle>
 
                 <PromptTitle 
                     title="${ constants.machining }" 
-                    visibility={ this.props.visibility.machining }>
+                    visibility={ visibility.machining }>
 
                     <PromptCheckboxGroup
                         name="machining"
-                        value={ this.props.state.machining }
-                        handleChange={ this.props.handleProcessingModuleChange }
+                        value={ state.machining }
+                        handleChange={ handleProcessingModuleChange }
                         foreignKeys={ foreignKeys.processing.machining } />
 
                     <PromptTextArea 
                         name="machining_note"
-                        value={ this.props.state.machining_note }
+                        value={ state.machining_note }
                         labelVisibility="${ htmlClass.visibility.on }"
-                        handleChange={ this.props.handleProcessingModuleChange } />
+                        handleChange={ handleProcessingModuleChange } />
                 </PromptTitle>
 
                 <PromptTitle 
                     title="${ constants.joining }" 
-                    visibility={ this.props.visibility.joining }>
+                    visibility={ visibility.joining }>
 
                     <PromptCheckboxGroup
                         name="joining"
-                        value={ this.props.state.joining }
-                        handleChange={ this.props.handleProcessingModuleChange }
+                        value={ state.joining }
+                        handleChange={ handleProcessingModuleChange }
                         foreignKeys={ foreignKeys.processing.joining } />
 
                     <PromptTextArea 
                         name="joining_note"
-                        value={ this.props.state.joining_note }
+                        value={ state.joining_note }
                         labelVisibility="${ htmlClass.visibility.on }"
-                        handleChange={ this.props.handleProcessingModuleChange } />
+                        handleChange={ handleProcessingModuleChange } />
                 </PromptTitle>
 
                 <PromptTitle  
                     title="${ constants.manufacturer }" 
-                    visibility={ this.props.visibility.manufacturer }>
+                    visibility={ visibility.manufacturer }>
 
                     <PromptTextArea 
                         name="manufacturer"
-                        value={ this.props.state.manufacturer }
+                        value={ state.manufacturer }
                         labelVisibility="${ htmlClass.visibility.off }"
-                        handleChange={ this.props.handleProcessingModuleChange } />
+                        handleChange={ handleProcessingModuleChange } />
                 </PromptTitle>
 
                 <PromptTitle  
                     title="${ constants.date }" 
-                    visibility={ this.props.visibility.date }>
+                    visibility={ visibility.date }>
 
                     <PromptTextArea 
                         name="date"
-                        value={ this.props.state.date }
+                        value={ state.date }
                         labelVisibility="${ htmlClass.visibility.off }"
-                        handleChange={ this.props.handleProcessingModuleChange } />
+                        handleChange={ handleProcessingModuleChange } />
                 </PromptTitle>
 
                 <PromptTitle  
                     title="${ constants.location }" 
-                    visibility={ this.props.visibility.location }>
+                    visibility={ visibility.location }>
 
                     <PromptTextArea 
                         name="location"
-                        value={ this.props.state.location }
+                        value={ state.location }
                         labelVisibility="${ htmlClass.visibility.off }"
-                        handleChange={ this.props.handleProcessingModuleChange } />
+                        handleChange={ handleProcessingModuleChange } />
                 </PromptTitle>
 
                 <PromptTitle  
                     title="${ constants.plant }" 
-                    visibility={ this.props.visibility.plant }>
+                    visibility={ visibility.plant }>
 
                     <PromptTextArea 
                         name="plant"
-                        value={ this.props.state.plant }
+                        value={ state.plant }
                         labelVisibility="${ htmlClass.visibility.off }"
-                        handleChange={ this.props.handleProcessingModuleChange } />
+                        handleChange={ handleProcessingModuleChange } />
                 </PromptTitle>
 
             </div>`
