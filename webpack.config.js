98f7391f2253210430fee9855b193bd9fbb1cc62
Jolene Borrelli <Jaomin@j-bs-macbook.local>
2020-05-06 16:36:21 -0600

    Added CSS and Style loaders to Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -38,6 +38,14 @@ module.exports =
                         loader: "html-loader"
                     }
                 ]
+            },
+            {
+                test: /\.css$/,
+                use: 
+                [
+                    'style-loader',
+                    'css-loader'
+                ]
             }
         ]
     },
