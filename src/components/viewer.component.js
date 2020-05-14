425088dad64cf2ece0a5a18ea8420e5267f0bc76
Jolene Borrelli <Jaomin@j-bs-macbook.local>
2020-05-14 14:37:15 -0600

    Issue #26 Add domain to path and set as uri

--- src/components/viewer.component.js
+++ src/components/viewer.component.js
@@ -1,4 +1,5 @@
 import React                from 'react';
+import { domain }           from '../constants/database.constants.js';
 import { genericHtmlClass } from '../constants/htmlClass.constants.js';
 
 export default class Viewer extends React.Component
@@ -12,15 +13,17 @@ export default class Viewer extends React.Component
     {
         const 
         { 
-            manifest,
+            path,
             children 
         } = this.props;
+
+        const uri = domain + path;
 
     return (
             <div 
                 className="uv" 
                 data-config=""
-                data-uri={ manifest }>
+                data-uri={ uri }>
                 { children }
             </div>
         )
