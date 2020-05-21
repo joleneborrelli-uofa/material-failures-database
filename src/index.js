aa35265d8027bc8f9211c050ccf53637d80000bd
Jolene Borrelli <Jaomin@j-bs-macbook.local>
2020-05-21 17:52:06 -0600

    Issue #33 Migrate to create-react-app, failed pull request?

--- src/index.js
+++
@@ -1,8 +0,0 @@
-// CSS
-import '../css/main.css';
-
-import React    from 'react';
-import ReactDOM from 'react-dom';
-import Router from './components/router.component.js';
-
-ReactDOM.render( <Router />, document.getElementById( 'container' ) );
