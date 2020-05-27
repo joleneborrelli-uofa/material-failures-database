4ca32ab7be6f8028cdc48c1a2cb4167f62ae4909
Jolene Borrelli <Jaomin@j-bs-macbook.local>
2020-05-27 14:37:03 -0600

    Delete mock database response

--- app/src/database/display.database.js
+++
@@ -1,9 +0,0 @@
-export const displayDatabase =
-[
-    {
-        "object_id"  : "1",
-        "case_study" : "on",
-        "record"     : "on",
-        "thumbnail"  : "/IIIF-image-server/img/derivatives/iiif/images/exhibit_6_5/full/250,/0/default.jpg"
-    }
-];
