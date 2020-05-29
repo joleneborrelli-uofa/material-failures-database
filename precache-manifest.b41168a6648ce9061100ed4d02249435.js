525ee4954afd6a4b7e8e441e3b4a7aaf3eff1697
Jolene Borrelli <Jaomin@j-bs-macbook.local>
2020-05-29 11:14:03 -0600

    Updates

---
+++ precache-manifest.b41168a6648ce9061100ed4d02249435.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "790cd18108cd9e110b6cf2f9bad6109d",
+    "url": "/material-failures-database/index.html"
+  },
+  {
+    "revision": "1f23c6bd22c0b33ebcbb",
+    "url": "/material-failures-database/static/css/main.6069cca8.chunk.css"
+  },
+  {
+    "revision": "7e5a528bbb3a761ec0da",
+    "url": "/material-failures-database/static/js/2.747dbdd0.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/material-failures-database/static/js/2.747dbdd0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f23c6bd22c0b33ebcbb",
+    "url": "/material-failures-database/static/js/main.219d93cf.chunk.js"
+  },
+  {
+    "revision": "c1c5ebac4845516909c2",
+    "url": "/material-failures-database/static/js/runtime-main.9a3fe691.js"
+  },
+  {
+    "revision": "1282c618874d13f2e6d97089f58b58cd",
+    "url": "/material-failures-database/static/media/globe.1282c618.png"
+  },
+  {
+    "revision": "5a3f9e947ff776b8d1c78448961e0a92",
+    "url": "/material-failures-database/static/media/unsolved.5a3f9e94.png"
+  }
+]);
